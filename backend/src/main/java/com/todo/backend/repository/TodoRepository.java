03f3889f973479514fedb3c79a7315f7194b2990
Sirisooriya S.T.N <sirisooriyastn.23@uom.lk>
2025-10-26 16:13:23 +0530

    Update TodoRepository.java

--- backend/src/main/java/com/todo/backend/repository/TodoRepository.java
+++ backend/src/main/java/com/todo/backend/repository/TodoRepository.java
@@ -6,13 +6,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    // Get tasks for a specific day
     List<Todo> findByDate(LocalDate date);
-
-    // Get overdue tasks (date before the given date)
     List<Todo> findByDateBefore(LocalDate date);
-
-    // Get upcoming tasks (date after the given date)
     List<Todo> findByDateAfter(LocalDate date);
 }
 
